8b8fc6ac5217aa16352770204c755626db4e1974
Nataliia Fedoryka <fedorykanataliya@gmail.com>
2024-04-13 23:41:40 +0300

    hw2

--- JS/task-4.js
+++ JS/task-4.js
@@ -24,6 +24,7 @@ break;
         break;
 } }
 
+
     
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
